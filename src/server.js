3c054a7ef5f5c52e0105370a79c7705abedd7f9a
Harshit-3905 <harshitjoshileet@gmail.com>
2024-01-27 21:50:13 +0530

    Update dev script and add server.js file

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
 import dotenv from "dotenv";
-import connectDB from "./src/db/db.js";
+import connectDB from "./db/db.js";
 import express from "express";
-import userRoutes from "./src/routes/userRoutes.js";
-import chatRoutes from "./src/routes/chatRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import chatRoutes from "./routes/chatRoutes.js";
 import cors from "cors";
-import requireAuth from "./src/middleware/requireAuth.js";
+import requireAuth from "./middleware/requireAuth.js";
 
 dotenv.config();
 const app = express();
@@ -16,7 +16,7 @@ app.use(express.json());
 const port = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+    res.send("Hello World");
 });
 
 app.use("/api/user", userRoutes);
@@ -24,5 +24,5 @@ app.use(requireAuth);
 app.use("/api/chat", chatRoutes);
 
 app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
+    console.log(`Server running on port http://localhost:${port}`);
 });
